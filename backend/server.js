35a30a591b6793d0ec92c2ae3bdbba9e60e91900
Najeeb Al-Selwi <n.selwi@gmx.de>
2020-12-14 14:24:34 +0100

    conect the server with mondoDB

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+ 
+app.get('/' , (request , response)=>{
+   response.send({ msg : 'welcome to Event Manager App' })
+})
+app.use(express.json())
+// app.use('/user' , require('./routes/user'))
+
+// connect to mongodb 
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.MONGO_URI}/${process.env.DB_NAME}?retryWrites=true&w=majority` , { useNewUrlParser: true , useUnifiedTopology: true })
+.then(()=>{
+    console.log('MonogoDB is connected 😎');
+})
+.catch((err)=>{
+   console.log(err);
+}) 
+
+
+
+app.listen(4000 , ()=>{
+    console.log('Server Started on port 4000');
+})
+
