1c2697e794b60abeeb9f5be8704b75938f7ed0f5
Najeeb Al-Selwi <n.selwi@gmx.de>
2020-12-14 15:05:25 +0100

    create gitignore file

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,26 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// dot .env.config();
- 
-app.get('/' , (request , response)=>{
-   response.send({ msg : 'welcome to Event Manager App' })
+dotenv.config();
+
+app.get('/', (request, response) => {
+    response.send({ msg: 'welcome to Event Manager App' })
 })
 app.use(express.json())
 // app.use('/user' , require('./routes/user'))
 
 // connect to mongodb 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.MONGO_URI}/${process.env.DB_NAME}?retryWrites=true&w=majority` , { useNewUrlParser: true , useUnifiedTopology: true })
-.then(()=>{
-    console.log('MonogoDB is connected 😎');
-})
-.catch((err)=>{
-   console.log(err);
-}) 
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.MONGO_URI}/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MonogoDB is connected 😎');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 
 
-app.listen(4000 , ()=>{
+app.listen(4000, () => {
     console.log('Server Started on port 4000');
 })
 
