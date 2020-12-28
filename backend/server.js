48d0cc6fb3b683e4ae4bfe9893d88579f230f33a
Jasmin Mansouri <jasmin.dci123@gmail.com>
2020-12-28 11:33:23 +0100

    updated

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.get('/', (request, response) => {
 })
 app.use(express.json())
 app.use('/contact' , require('./Routes/Contact'))
-app.use('/event',require('./Routes/EventRoute'))
+app.use('/startEvent',require('./Routes/EventRoute'))
 
 // connect to mongodb 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.MONGO_URI}/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
