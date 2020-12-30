5550c9ce794e3191ccbb35cca1f4dcdeefc8568c
Najeeb Al-Selwi <n.selwi@gmx.de>
2020-12-30 12:14:11 +0100

    updating CategoryRoute

--- backend/Routes/CategoryRoute.js
+++ backend/Routes/CategoryRoute.js
@@ -4,8 +4,8 @@ const Events = require('../Models/CategoryModel');
 
 const router = express.Router();
 
-
-router.post('/', async (request, response) => {
+// Adding new Categories
+router.post('/add', async (request, response) => {
 
     const { name, description } = request.body;
     const event = new Events({
@@ -13,7 +13,33 @@ router.post('/', async (request, response) => {
     });
     await event.save();
 
-    response.send('you have created your Categeory ')
+    response.send(`${name} Categeory have been created !!`)
 });
+
+router.put('/edit/:id', (request, response) => {
+    try {
+        EventsMgr.collection('Category').updateOne({ _id: new ObjectID(request.params.id) }, {
+            $set: { name: 'Najeeb' }
+        });
+        response.send('Category have been updated!!')
+    } catch (err) {
+        console.log(err);
+        response.send('server error');
+    }
+})
+
+router.delete('/delete/:id', (request, response) => {
+    try {
+        EventsMgr.collection('Category').deleteOne({ _id: new ObjectID(request.params.id) }, {
+            $set: { name: 'js' }
+        });
+        response.send('Category have been Deleted!!')
+    } catch (err) {
+        console.log(err);
+        response.send('server error');
+    }
+
+})
+
 
 module.exports = router
