daad5b6394bb967a86a9d499f50c9f20d40efec2
Najeeb Al-Selwi <n.selwi@gmx.de>
2020-12-31 12:51:00 +0100

    updating  header conmponent and App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './Components/Home';
+import Header from './Components/Header';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+    <Router>
+      <Header />
+      <Switch>
+        <Route path='/' component={Home} exact />
+        {/* <Route path='/contact' component={Contact} />
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/help' component={Help} />
+        <Route component={Error} /> */}
+      </Switch>
+    </Router>  );
 }
 
 export default App;
