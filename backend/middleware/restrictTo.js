920467e181ba7e8373bcbe85b15762a9d3c7c24c
Jasmin Mansouri <jasmin.dci123@gmail.com>
2021-01-04 14:43:34 +0100

    updated

--- backend/middleware/restrictTo.js
+++ backend/middleware/restrictTo.js
@@ -1,13 +1,17 @@
-const { response } = require("express")
+const User = require("../Models/UserModel")
 
-module.exports = (...roles)=>{
-    return(request, response,next)=>{
+const restrictTo =  (...roles)=>{
+    return async(request, response,next)=>{
 
     
     // roles is an array ['admin' ,'supervisor']
-    if(!roles.includes(request.user.role)){
+    const user = await User.findById(request.id).select('-password');
+
+    if(!roles.includes(user.role)){
         return response.status(404).json({msg: 'You dont have the permission for this action, Authorization Denied'})
     }
     next()
     }
 }
+
+module.exports = restrictTo;
--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ app.use(express.json())
 app.use('/contact', require('./Routes/ContactRoute'));
 app.use('/category', require('./Routes/CategoryRoute'));
 app.use('/user', require('./Routes/UserRoute'));
-app.use('/event', require('./Routes/EventRoute'))
+app.use('/event', require('./Routes/EventRoute'));
+app.use('/admin', require('./Routes/AdminRoute'))
 
 
 
