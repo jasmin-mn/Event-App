0f365c7d469d16403829df858409a2911e14877b
Najeeb Al-Selwi <n.selwi@gmx.de>
2021-01-04 11:57:46 +0100

    Updating Footer

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './Footer.module.css';
 
 
 export default function Footer() {
 
     return (
-        <div id='footer'>
+        <div className={styles.footer}>
             <p>footer</p>
         </div>
     )
