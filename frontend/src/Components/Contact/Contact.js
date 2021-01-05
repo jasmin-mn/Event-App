e9335a454d33112782d11587147ecfbaa7eebcbd
Najeeb Al-Selwi <n.selwi@gmx.de>
2021-01-05 11:07:46 +0100

    creating contact components files

---
+++ frontend/src/Components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styles from './Contact.module.css';
+
+export default function Header() {
+
+    return (
+        <div className={styles.contact}>
+
+        </div>
+    )
+}
--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import styles from './Home.module.css';
 
 
 function Home() {
     return (
-        <div>
-           <h1> Upcoming Events</h1> 
-           <p> See all Events</p>
+        <div className={styles.main}>
+
+            <div className={styles.upcoming_envents}>
+                <div className={styles.upcoming_envents_head}>
+                    <h2> Upcoming Events</h2>
+                    <p>See all Events</p>
+                </div>
+
+                <div className={styles.upcoming_envents_body}>
+
+                </div>
+            </div>
+
+
+            <div className={styles.envents_by_city}>
+                <div className={styles.envents_by_city_head}>
+                    <h2> Events by City</h2>
+                    <p>See all Events</p>
+                </div>
+
+                <div className={styles.envents_by_citys_body}>
+
+                </div>
+            </div>
+
+
+            <div className={styles.envents_by_Category}>
+                <div className={styles.envents_by_Category_head}>
+                    <h2> Events by Category</h2>
+                    <p>See all Events</p>
+                </div>
+
+                <div className={styles.envents_by_Category_body}>
+
+                </div>
+            </div>
+
         </div>
     )
 }
