9234561ddd514a92c8a7f67bb48e75a5eb4c929f
Najeeb Al-Selwi <n.selwi@gmx.de>
2021-01-06 12:49:50 +0100

    updating the footer

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -6,7 +6,10 @@ export default function Footer() {
 
     return (
         <div className={styles.footer}>
-            <p>footer</p>
+            <p><a>Home</a> | <a>Login</a> | <a>Sign up</a> | <a>Contact us</a> | <a>About</a> | <a>Terms of Use</a></p>
+            <p>&copy; 2021 EventsMGR Team</p>
         </div>
     )
 }
+
+
