24c620c98b72d06670b736ef70b03972f036d0f4
Jasmin Mansouri <jasmin.dci123@gmail.com>
2021-01-13 14:47:14 +0100

    updated

--- frontend/src/Components/UserPage/UserPage.js
+++ frontend/src/Components/UserPage/UserPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function UserPage() {
     return (
         <div>
-           <h1>LoggedIn </h1> 
+           <h1>LoggedIn User </h1> 
         </div>
     )
 }
