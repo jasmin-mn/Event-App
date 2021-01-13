5fed6400be2828cf9605a1e012628876e427277a
Najeeb Al-Selwi <n.selwi@gmx.de>
2021-01-13 21:07:02 +0100

    styling the header

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -8,83 +8,10 @@ export default function Header() {
 
     const { loggedInState } = useContext(UserStateContext);
 
-    //     return (
-
-    //         <div className={styles.header}>
-
-    //             <div className={styles.nav}>
-    //                 <img id="logo" src={logo} alt={"logo"} />
-
-    //                 {loggedInState ? (
-
-    //                     <ul className={styles.nav_list}>
-    //                         <li>
-    //                             <Link to={'/'}>Home</Link>
-    //                         </li>
-
-    //                         <li>
-    //                             <Link to={'/Profile'}>Profile</Link>
-
-    //                         </li>
-
-    //                         <li>
-    //                             <Link to={'/Messages'}>Messages</Link>
-    //                         </li>
-    //                         <li>
-    //                             <Link to={'/notification'}>Notification</Link>
-    //                         </li>
-
-    //                         <li>
-    //                             <Link to={'/contact'}>Contact</Link>
-    //                         </li>
-
-    //                     </ul>
-
-    //                 ) : (
-    //                         <>
-    //                             <ul className={styles.nav_list}>
-    //                                 <li>
-    //                                     <Link to={'/'}>Home</Link>
-    //                                 </li>
-
-    //                                 <li>
-    //                                     <Link to={'/about'}>About us</Link>
-    //                                 </li>
-
-    //                                 <li>
-    //                                     <Link to={'/login'}>Login</Link>
-    //                                 </li>
-    //                                 <li>
-    //                                     <Link to={'/signup'}>Signup</Link>
-    //                                 </li>
-
-    //                                 <li>
-    //                                     <Link to={'/contact'}>Contact us</Link>
-    //                                 </li>
-
-    //                             </ul>
-
-    //                             <div className={styles.header_bg}>
-    //                                 <div className={styles.header_text}>
-    //                                     <p>Enjoy fun Coding events, learning workshops classes from home</p>
-    //                                     <button className={styles.btn}>Join us</button>
-    //                                 </div>
-    //                             </div>
-
-    //                         </>
-    //                     )
-    //                 }
-    //             </div>
-    //         </div>
-    //     )
-    // }
-
     if (loggedInState) {
 
         return (
-
             <div className={styles.header}>
-
                 <div className={styles.nav}>
                     <img id="logo" src={logo} alt={"logo"} />
 
@@ -95,7 +22,6 @@ export default function Header() {
 
                         <li>
                             <Link to={'/Profile'}>Profile</Link>
-
                         </li>
 
                         <li>
@@ -108,9 +34,7 @@ export default function Header() {
                         <li>
                             <Link to={'/contact'}>Contact</Link>
                         </li>
-
                     </ul>
-
                 </div>
             </div>
         )
@@ -118,43 +42,36 @@ export default function Header() {
     } else {
 
         return (
-            <>
-                <div className={styles.header}>
+            <div className={styles.header}>
+                <div className={styles.nav}>
+                    <img id="logo" src={logo} alt={"logo"} />
 
-                    <div className={styles.nav}>
-                        <img id="logo" src={logo} alt={"logo"} />
-
-                        <ul className={styles.nav_list}>
-                            <li>
-                                <Link to={'/'}>Home</Link>
-                            </li>
-
-                            <li>
-                                <Link to={'/about'}>About us</Link>
-                            </li>
-
-                            <li>
-                                <Link to={'/login'}>Login</Link>
-                            </li>
-                            <li>
-                                <Link to={'/signup'}>Signup</Link>
-                            </li>
-
-                            <li>
-                                <Link to={'/contact'}>Contact us</Link>
-                            </li>
-
-                        </ul>
-                    </div>
-
-                    <div className={styles.header_bg}>
-                        <div className={styles.header_text}>
-                            <p className={styles.header_tittel}>Enjoy Coding Events, <br/> learning, Workshops Classes from Home.</p>
-                            <button className={styles.btn}>Join us</button>
-                        </div>
-                    </div>
-
+                    <ul className={styles.nav_list}>
+                        <li>
+                            <Link to={'/'}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to={'/about'}>About us</Link>
+                        </li>
+                        <li>
+                            <Link to={'/login'}>Login</Link>
+                        </li>
+                        <li>
+                            <Link to={'/signup'}>Signup</Link>
+                        </li>
+                        <li>
+                            <Link to={'/contact'}>Contact us</Link>
+                        </li>
+                    </ul>
                 </div>
-            </>)
+
+                <div className={styles.header_bg}>
+                    <div className={styles.header_text}>
+                        <p className={styles.header_tittel}>Enjoy Coding Events, <br /> learning, Workshops Classes from Home.</p>
+                        <button className={styles.btn}>Join us</button>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
