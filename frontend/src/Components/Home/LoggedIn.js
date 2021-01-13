b7b5b8143930affd87cfe6774e0234cdce13b11a
Jasmin Mansouri <jasmin.dci123@gmail.com>
2021-01-13 14:18:01 +0100

    updated

---
+++ frontend/src/Components/Home/LoggedIn.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react'
+import Search from '../Search/Search';
+import styles from './Home.module.css';
+import axios from "axios";
+
+
+function HomeLoggedIn() {
+
+    
+
+    return (
+        <></>
+    )
+}
+
+export default HomeLoggedIn
