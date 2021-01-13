62ce576cbe7399dbe8f234d560dd6b3fc437648a
Jasmin Mansouri <jasmin.dci123@gmail.com>
2021-01-13 14:35:31 +0100

    updated

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Search from '../Search/Search';
 import styles from './Home.module.css';
-import axios from "axios";
+
 
 import HomeDefault from './Default';
 import HomeLoggedIn from './LoggedIn';
