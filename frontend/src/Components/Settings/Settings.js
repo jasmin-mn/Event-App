1164ff23520c3349a319b869fc42f50c4db4e3d9
Af <af_ajay@yahoo.com>
2021-01-28 15:42:41 +0100

    Profile update solve

--- frontend/src/Components/Settings/Settings.js
+++ frontend/src/Components/Settings/Settings.js
@@ -6,17 +6,17 @@ const Settings = () => {
   const [deleteId, setDeleteId] = useState()
 
   useEffect(()=>{
-    // getData() 
+    getData() 
 
   },[])
-  //  const getData = async()=>{
-  //    const response = await axios.get("http://localhost:7000/user/profile")
-  //    setDeleteId(response.data)
-  //  }
-  const userUrl = 'http://localhost:7000/user/login'
+   const getData = async(deleteData)=>{
+     const response = await axios.get("http://localhost:7000/user/profile")
+     setDeleteId(response.data)
+   }
+  
 
    const removeData = (id)=>{
-     axios.delete(`${"http://localhost:7000/user/deleteAccount"}/${id}`).then(response =>{
+     axios.delete("http://localhost:7000/user/deleteAccount").then(response =>{
        const del = deleteId.filter(deleteId=> id !== deleteId.id )
        setDeleteId(del)
 
@@ -30,7 +30,7 @@ const Settings = () => {
 
   return (
     <div className={styles.containersetting}>
-      {" "}
+      
       Settings
       <div className={styles.deletecontainer}>
         <p>
