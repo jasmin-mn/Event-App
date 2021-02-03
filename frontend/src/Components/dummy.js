1fd5269dd25ed7ca0fbcea969fdd8fd95e77eaa3
Af <af_ajay@yahoo.com>
2021-02-03 09:39:28 +0100

    checking the previous code

---
+++ frontend/src/Components/dummy.js
@@ -0,0 +1,30 @@
+/// authenticate
+const jwt = require("jsonwebtoken")
+const User = require("../Models/UserModel")
+
+module.exports = async(request,response,next)=>{
+    const token = request.header('authorization')
+    console.log(token);
+
+    if(!token){
+        return response.status(400).json({msg: 'No Token authorization denied!!'})
+    }
+    try{
+        const decode = await jwt.verify(token, process.env.SECRET)
+        console.log('decode:', decode);
+        request.id = decode.id
+        const currentUser = await User.findById(decode.id)
+        if(!currentUser){
+            return response.status(400).json({msg: 'The User with the current token is no longer exist'})
+        }
+        request.id = currentUser.id
+        console.log(request.user);
+        next()
+    } catch(error){
+        console.log(error);
+        return response.status(400).json({msg: 'Token is not valid'})
+    }
+}
+
+
+/////end authenticate
