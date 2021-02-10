fc730d41f0011b893b9ae6c6b7b097ccafbdc7f5
Af <af_ajay@yahoo.com>
2021-02-10 18:06:56 +0100

    added modalbox in resetpassword

--- frontend/src/Components/Resetpasswordpage/Resetpasswordpage.js
+++ frontend/src/Components/Resetpasswordpage/Resetpasswordpage.js
@@ -1,103 +1,106 @@
 import React, { useState, useEffect, useContext } from "react";
 import styles from "./Resetpasswordpage.module.css";
-import axios from 'axios';
-import {useParams} from 'react-router-dom'
-import {ModalBoxContext} from '../ModalBox/ModalBox'
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { ModalBoxContext } from "../ModalBox/ModalBox";
 
 function Resetpasswordpage() {
-  const {token} = useParams()
-  const [validToken, setValidToken ] = useState(false); 
-  const [user, setUser ] = useState({password:"",password2:"", token:""}); 
-  const {addModalBox} = useContext(ModalBoxContext)
+  const { token } = useParams();
+  const [validToken, setValidToken] = useState(false);
+  const [user, setUser] = useState({ password: "", password2: "", token: "" });
+  const { addModalBox } = useContext(ModalBoxContext);
 
-  const sendResetToken = async () =>{
-  
+  const sendResetToken = async () => {
     try {
-      const response = await axios.get(`http://localhost:7000/user/newPassword/${token}` )
+      const response = await axios.get(
+        `http://localhost:7000/user/newPassword/${token}`
+      );
       console.log(response.data);
-      setValidToken(true)
-      setUser({...user, token})
+      setValidToken(true);
+      setUser({ ...user, token });
     } catch (error) {
       console.log(error.response.data);
-      setValidToken(false)
+      setValidToken(false);
     }
-
-  }
+  };
 
   useEffect(() => {
-    if(token)  sendResetToken();
-   
-  }, [])
+    if (token) sendResetToken();
+  }, []);
 
   const [password, setPassword] = useState("");
   const changePw = (e) => {
-    setUser({...user,[e.target.name]:e.target.value})
-  }
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
   const sendResetPw = async (e) => {
-    e.preventDefault()
-    const {password, password2} = user
-    if (password === ""|| password2 === "") {
+    e.preventDefault();
+    const { password, password2 } = user;
+    if (password === "" || password2 === "") {
       console.log("please enter all the fields", " alert-infos");
-  } else if (password !== password2) {
-    console.log("password do not match", " alert-infos");
-  } else if (password.length < 7 || password2.length < 7) {
+    } else if (password !== password2) {
+      console.log("password do not match", " alert-infos");
+    } else if (password.length < 7 || password2.length < 7) {
       console.log("password must be more than 7 Character", " alert-infos");
-    
-  }
-  else{
-    try {
-      const config = {
-        headers: { 'Content-Type': 'application/json' }
-  
-      };
-      const response = await axios.post('http://localhost:7000/user/newPassword', user,config)
-      console.log(response.data.msg);
-      addModalBox("You have successfully reset your password")
-      window.location = '/login'
-      
-    } catch (error) {
-      console.log(error.response.data.msg);
-      
+    } else {
+      try {
+        const config = {
+          headers: { "Content-Type": "application/json" },
+        };
+        const response = await axios.post(
+          "http://localhost:7000/user/newPassword",
+          user,
+          config
+        );
+        console.log(response.data.msg);
+        window.location = "/login";
+      } catch (error) {
+        console.log(error.response.data.msg);
+      }
     }
-  }
-
-    
-    // try {
-    //   const result = await axios.post('http://localhost:7000/user/resetPassword', data, config);
-    //   console.log(result);
-    // } catch (error) {
-    //   console.log(error);
-    // }
-  }
-  // const submitResetpw = async (e) => {
-  //   e.preventDefault();
-  //   sendResetPw({ password })
-
-  // }
-
+  };
 
   return (
     <div className={styles.containerresetpw}>
       <h1>Resetpassword</h1>
-      { validToken ?
+      {validToken ? (
+        <form onSubmit={sendResetPw} className={styles.formpasswordheader}>
+          <div className={styles.formpassword}>
+            <label htmlFor="password">
+              <i class="icon-shield"></i>
+            </label>
+            <input
+              className={styles.forminput}
+              type="password"
+              onChange={changePw}
+              name="password"
+              placeholder=" Enter your new Password"
+            />
+          </div>
 
-      <form onSubmit={sendResetPw} className={styles.formpasswordheader}>
-        <div className={styles.formpassword}>
-          <label htmlFor="password"><i class="icon-shield"></i></label>
-          <input type="password" onChange={changePw} name="password" placeholder=" Enter your new Password" />
-        </div>
-
-        <div className={styles.formpasswordnew}>
-          <label htmlFor="password"><i class="icon-shield"></i></label>
-          <input type="password" onChange={changePw} name="password2" placeholder=" Confirm your new Password" />
-        </div>
-        <input type="submit" value="Resetpassword" className={styles.submitresetpw} />
-
-      </form> :
-      <h1><span>Your token is not valid </span> </h1> }
+          <div className={styles.formpasswordnew}>
+            <label htmlFor="password">
+              <i class="icon-shield"></i>
+            </label>
+            <input
+              type="password"
+              onChange={changePw}
+              name="password2"
+              placeholder=" Confirm your new Password"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Resetpassword"
+            className={styles.submitresetpw}
+          />
+        </form>
+      ) : (
+        <h1>
+          <span>Your token is not valid </span>{" "}
+        </h1>
+      )}
     </div>
   );
-
 }
 export default Resetpasswordpage;
